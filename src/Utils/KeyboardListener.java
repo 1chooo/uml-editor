853e292860137ffdae7a4060e93ae8629c691ec7
1chooo <hugo970217@gmail.com>
2024-03-29 00:11:32 +0800

    feat: Closs App Listener

---
+++ src/Utils/KeyboardListener.java
@@ -0,0 +1,24 @@
+// src/Utils/KeyboardListener.java
+
+package src.Utils;
+
+import java.awt.event.KeyEvent;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+
+public class KeyboardListener {
+    public KeyboardListener() {    }
+
+    public void registerKeyboardListener() {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent e) {
+                // Check if the user pressed Ctrl+Q (for Windows) or Command+Q (for Mac)
+                if ((e.getKeyCode() == KeyEvent.VK_Q) &&
+                        ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0))
+                    System.exit(StatusCode.EXIT);
+                return false;
+            }
+        });
+    }
+}
--- src/Views/Editor.java
+++ src/Views/Editor.java
@@ -4,37 +4,24 @@ package src.Views;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 import src.Components.MenuBar;
 import src.Components.SidePanel;
-import src.Utils.StatusCode;
+import src.Utils.KeyboardListener;
 
 public class Editor extends JFrame {
+    // private variables
     private int width = 1280;
     private int height = 720;
 
+    // constructor
     public Editor() {
         super("XML Editor");
 
         initComponents();
-
-        setSize(width, height);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Enroll the Keyboard Listener
-        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
-            @Override
-            public boolean dispatchKeyEvent(KeyEvent e) {
-                // Check if the user pressed Ctrl+Q (for Windows) or Command+Q (for Mac)
-                if ((e.getKeyCode() == KeyEvent.VK_Q) &&
-                        ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0))
-                    System.exit(StatusCode.EXIT);
-                return false;
-            }
-        });
     }
 
+    // private methods
     private void initComponents() {
         setLayout(new BorderLayout());
 
@@ -43,6 +30,11 @@ public class Editor extends JFrame {
 
         JPanel sidePanel = new SidePanel();
         add(sidePanel, BorderLayout.WEST);
-    }
 
+        KeyboardListener keyboardListener = new KeyboardListener();
+        keyboardListener.registerKeyboardListener();
+
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
