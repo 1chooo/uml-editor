ad4b5c533e86d54795bf91dae362412575d84f05
1chooo <hugo970217@gmail.com>
2024-04-01 13:04:27 +0800

    fix: deploy to gh-pages #1

--- src/Components/ModeStatus.java
+++ src/Components/ModeStatus.java
@@ -3,10 +3,12 @@ package src.Components;
 import javax.swing.JLabel;
 
 public class ModeStatus extends JLabel {
+    // Constructor
     public ModeStatus() {
         initComponents();
     }
 
+    // Private methods
     private void initComponents() {
         setText("Mode: Select");
     }
