b32eda99bed2c0c952c4b19540c8fdae1691378d
1chooo <hugo970217@gmail.com>
2024-04-10 22:36:53 +0800

    feat: Modes

--- src/Modes/ClassMode.java
+++ src/Modes/ClassMode.java
@@ -1,5 +1,15 @@
 package src.Modes;
 
-public class ClassMode {
-    
+import java.awt.*;
+import java.awt.event.*;
+
+public class ClassMode extends Mode {
+    public ClassMode() {
+    }
+
+    public void mousePressed(MouseEvent e) {
+        System.out.println("x:" + e.getX() + "  y: " + e.getY());
+        Shape obj = new ClassObj(e.getX(), e.getY(), "Class");
+        canvas.addShape(obj);
+    }
 }
--- src/Modes/Mode.java
+++ src/Modes/Mode.java
@@ -2,7 +2,31 @@ package src.Modes;
 
 import src.Components.Canvas;
 
-public interface Mode {
-    void activate(Canvas canvas);
-    void deactivate(Canvas canvas);
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public abstract class Mode implements MouseListener, MouseMotionListener {
+    protected Canvas canvas = Canvas.getInstance();
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
 }
