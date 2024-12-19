71c03c138050379d203690b46c554e11eea59977
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-19 15:17:05 +0800

    refactor: change string constants to final in CONFIG class for better immutability

--- src/utils/CONFIG.java
+++ src/utils/CONFIG.java
@@ -1,15 +1,15 @@
 package utils;
 
 public class CONFIG {
-    public static String APP_NAME = "UML Editor";
-    public static String APP_VERSION = "Version";
-    public static String MENU_FILE = "File";
-    public static String MENU_EDIT = "Edit";
-    public static String MENU_ITEM_ABOUT = "About UML Editor";
-    public static String MENU_ITEM_QUIT = "Quit (Control + Q)";
-    public static String MENU_ITEM_CHANGE_OBJ_NAME = "Change Object Name";
-    public static String MENU_ITEM_GROUP = "Group";
-    public static String MENU_ITEM_UNGROUP = "UnGroup";
+    public static final String APP_NAME = "UML Editor";
+    public static final String APP_VERSION = "Version";
+    public static final String MENU_FILE = "File";
+    public static final String MENU_EDIT = "Edit";
+    public static final String MENU_ITEM_ABOUT = "About UML Editor";
+    public static final String MENU_ITEM_QUIT = "Quit (Control + Q)";
+    public static final String MENU_ITEM_CHANGE_OBJ_NAME = "Change Object Name";
+    public static final String MENU_ITEM_GROUP = "Group";
+    public static final String MENU_ITEM_UNGROUP = "UnGroup";
 
     private CONFIG() {
     }
